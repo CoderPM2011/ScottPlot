9258e9c65bf0fbd297b734627f5b5ea2a07944ff
PM2011 <coderPM2011@outlook.com>
2024-06-25 20:04:14 +0800

    tmp-test

--- src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
+++ src/ScottPlot5/ScottPlot5 Sandbox/Sandbox.WinForms/Form1.cs
@@ -8,7 +8,16 @@ public partial class Form1 : Form
     {
         InitializeComponent();
 
-        formsPlot1.Plot.Add.Signal(Generate.Sin());
-        formsPlot1.Plot.Add.Signal(Generate.Cos());
+        Coordinates[] points =
+        {
+            new (0, 0.25),
+            new (0.3, 0.75),
+            new (0.7, 0.5),
+            new (1, 1),
+        };
+
+        var poly = formsPlot1.Plot.Add.Scatter(points);
+        poly.FillY = true;
+        poly.FillYColor = Colors.Green;
     }
 }
