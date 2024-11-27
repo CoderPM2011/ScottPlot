dd967ea2e440d35c417e8785f076c9548f503221
PM2011 <coderPM2011@outlook.com>
2024-11-27 22:08:33 +0800

    CoordinateRangeMutable: Change the method of modifying the value to an extension method

--- src/ScottPlot5/ScottPlot5/Primitives/CoordinateRangeMutable.cs
+++ src/ScottPlot5/ScottPlot5/Primitives/CoordinateRangeMutable.cs
@@ -50,130 +50,11 @@ public sealed class CoordinateRangeMutable(double min, double max) : IEquatable<
         return TrueMin <= value && value <= TrueMax;
     }
 
-    // TODO: deprecate
-    /// <summary>
-    /// Expand the range if needed to include the given point
-    /// </summary>
-    public void Expand(double value)
-    {
-        if (double.IsNaN(value))
-            return;
-
-        if (double.IsNaN(Min) || value < Min)
-            Min = value;
-
-        if (double.IsNaN(Max) || value > Max)
-            Max = value;
-    }
-
-    // TODO: deprecate
-    /// <summary>
-    /// Expand this range if needed to ensure the given range is included
-    /// </summary>
-    public void Expand(CoordinateRangeMutable range)
-    {
-        Expand(range.Min);
-        Expand(range.Max);
-    }
-
-    /// <summary>
-    /// Expand this range if needed to ensure the given range is included
-    /// </summary>
-    public void Expand(CoordinateRange range)
-    {
-        Expand(range.Min);
-        Expand(range.Max);
-    }
-
     /// <summary>
     /// This magic value is used to indicate the range has not been set.
     /// It is equal to an inverted infinite range [∞, -∞]
     /// </summary>
     public static CoordinateRangeMutable NotSet => new(double.PositiveInfinity, double.NegativeInfinity);
-
-    // TODO: deprecate
-    /// <summary>
-    /// Reset this range to inverted infinite values to indicate the range has not yet been set
-    /// </summary>
-    public void Reset()
-    {
-        Min = double.PositiveInfinity;
-        Max = double.NegativeInfinity;
-        if (HasBeenSet)
-            throw new InvalidOperationException();
-    }
-
-    public void Set(double min, double max)
-    {
-        Min = min;
-        Max = max;
-    }
-
-    public void Set(CoordinateRange range)
-    {
-        if (range.IsInverted)
-        {
-            Max = range.Min;
-            Min = range.Max;
-        }
-        else
-        {
-            Min = range.Min;
-            Max = range.Max;
-        }
-    }
-
-    public void Set(CoordinateRangeMutable range)
-    {
-        Min = range.Min;
-        Max = range.Max;
-    }
-
-    public void Set(IAxis otherAxis)
-    {
-        Min = otherAxis.Min;
-        Max = otherAxis.Max;
-    }
-
-    public void Pan(double delta)
-    {
-        Min += delta;
-        Max += delta;
-    }
-
-    public void PanMouse(float mouseDeltaPx, float dataSizePx)
-    {
-        double pxPerUnitx = dataSizePx / Span;
-        double delta = mouseDeltaPx / pxPerUnitx;
-        Pan(delta);
-    }
-
-    public void ZoomFrac(double frac)
-    {
-        ZoomFrac(frac, Center);
-    }
-
-    public void ZoomOut(double multiple)
-    {
-        double newSpan = Span * multiple;
-        double halfSpan = newSpan / 2;
-        Set(Center - halfSpan, Center + halfSpan);
-    }
-
-    public void ZoomMouseDelta(float deltaPx, float dataSizePx)
-    {
-        double deltaFracX = deltaPx / (Math.Abs(deltaPx) + dataSizePx);
-        double fracX = Math.Pow(10, deltaFracX);
-        ZoomFrac(fracX);
-    }
-
-    public void ZoomFrac(double frac, double zoomTo)
-    {
-        double spanLeftX = zoomTo - Min;
-        double spanRightX = Max - zoomTo;
-        Min = zoomTo - spanLeftX / frac;
-        Max = zoomTo + spanRightX / frac;
-    }
 
     public bool Equals(CoordinateRangeMutable? other)
     {
@@ -210,5 +91,127 @@ public sealed class CoordinateRangeMutable(double min, double max) : IEquatable<
         hash = hash * 23 + Min.GetHashCode();
         hash = hash * 23 + Max.GetHashCode();
         return hash;
+    }
+}
+
+public static class CoordinateRangeMutableHelper
+{
+    // TODO: deprecate
+    /// <summary>
+    /// Expand the range if needed to include the given point
+    /// </summary>
+    public static void Expand(this CoordinateRangeMutable range, double value)
+    {
+        if (double.IsNaN(value))
+            return;
+
+        if (double.IsNaN(range.Min) || value < range.Min)
+            range.Min = value;
+
+        if (double.IsNaN(range.Max) || value > range.Max)
+            range.Max = value;
+    }
+
+    // TODO: deprecate
+    /// <summary>
+    /// Expand this range if needed to ensure the given range is included
+    /// </summary>
+    public static void Expand(this CoordinateRangeMutable range, CoordinateRangeMutable newRange)
+    {
+        range.Expand(newRange.Min);
+        range.Expand(newRange.Max);
+    }
+
+    /// <summary>
+    /// Expand this range if needed to ensure the given range is included
+    /// </summary>
+    public static void Expand(this CoordinateRangeMutable range, CoordinateRange newRange)
+    {
+        range.Expand(newRange.Min);
+        range.Expand(newRange.Max);
+    }
+
+    // TODO: deprecate
+    /// <summary>
+    /// Reset this range to inverted infinite values to indicate the range has not yet been set
+    /// </summary>
+    public static void Reset(this CoordinateRangeMutable range)
+    {
+        range.Min = double.PositiveInfinity;
+        range.Max = double.NegativeInfinity;
+        if (range.HasBeenSet)
+            throw new InvalidOperationException();
+    }
+
+    public static void Set(this CoordinateRangeMutable range, double min, double max)
+    {
+        range.Min = min;
+        range.Max = max;
+    }
+
+    public static void Set(this CoordinateRangeMutable range, CoordinateRange newRange)
+    {
+        if (newRange.IsInverted)
+        {
+            range.Max = newRange.Min;
+            range.Min = newRange.Max;
+        }
+        else
+        {
+            range.Min = newRange.Min;
+            range.Max = newRange.Max;
+        }
+    }
+
+    public static void Set(this CoordinateRangeMutable range, CoordinateRangeMutable newRange)
+    {
+        range.Min = newRange.Min;
+        range.Max = newRange.Max;
+    }
+
+    public static void Set(this CoordinateRangeMutable range, IAxis otherAxis)
+    {
+        range.Min = otherAxis.Min;
+        range.Max = otherAxis.Max;
+    }
+
+    public static void Pan(this CoordinateRangeMutable range, double delta)
+    {
+        range.Min += delta;
+        range.Max += delta;
+    }
+
+    public static void PanMouse(this CoordinateRangeMutable range, float mouseDeltaPx, float dataSizePx)
+    {
+        double pxPerUnitx = dataSizePx / range.Span;
+        double delta = mouseDeltaPx / pxPerUnitx;
+        range.Pan(delta);
+    }
+
+    public static void ZoomFrac(this CoordinateRangeMutable range, double frac)
+    {
+        range.ZoomFrac(frac, range.Center);
+    }
+
+    public static void ZoomOut(this CoordinateRangeMutable range, double multiple)
+    {
+        double newSpan = range.Span * multiple;
+        double halfSpan = newSpan / 2;
+        range.Set(range.Center - halfSpan, range.Center + halfSpan);
+    }
+
+    public static void ZoomMouseDelta(this CoordinateRangeMutable range, float deltaPx, float dataSizePx)
+    {
+        double deltaFracX = deltaPx / (Math.Abs(deltaPx) + dataSizePx);
+        double fracX = Math.Pow(10, deltaFracX);
+        range.ZoomFrac(fracX);
+    }
+
+    public static void ZoomFrac(this CoordinateRangeMutable range, double frac, double zoomTo)
+    {
+        double spanLeftX = zoomTo - range.Min;
+        double spanRightX = range.Max - zoomTo;
+        range.Min = zoomTo - spanLeftX / frac;
+        range.Max = zoomTo + spanRightX / frac;
     }
 }
